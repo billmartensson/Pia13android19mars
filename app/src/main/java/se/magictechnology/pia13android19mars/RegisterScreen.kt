34573cc39d978a4a4bd93bf3abbca607b3c365c0
Bill Mårtensson <bill@magictechnology.se>
2025-03-24 09:53:24 +0100

    register start

--- app/src/main/java/se/magictechnology/pia13android19mars/RegisterScreen.kt
+++ app/src/main/java/se/magictechnology/pia13android19mars/RegisterScreen.kt
@@ -1,2 +1,22 @@
 package se.magictechnology.pia13android19mars
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RegisterScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text("REGISTER")
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun RegisterScreenPreview() {
+    RegisterScreen()
+}
